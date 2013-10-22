544a4d50a4d92ab5ffda0dfc8cc4f98b3b5513eb
Sibrand Hoekstra <sibrand@gmail.com>
2013-10-22 23:46:28 +0200

    v0.2
    - Added ajax through jquery
    - bugs: different implementations of substr across browsers?
    - r,g are processed correctly in Chrome, b is off (values too low)
    - r, g result in NaN in FF, blue yields result (wrong: values tool low)
    
    - general code clearuppery

--- js/app.js
+++ js/app.js
@@ -1,26 +1,37 @@
+var hexcolors = [];
+var rgbcolors = [];
+var palettename;
+var svgfileurl;
+var svgfile;
 
-function convertColors() {
-	var hexcolors = [];
-	var rgbcolors = [];
+function init() {
+$('#loadFile').click(function(){
+	svgfileurl = $("#svgFile").val();
+	loadFile(svgfileurl);
+});
+}
+
+function convertColors(svgfile) {
+	
 
 	//TODO: implement ajax request for remote svg files
 	//TODO: implement sever to accept file uploads
-	var svg = document.getElementsByTagName('svg')[0];
-	var boxes = svg.getElementsByTagName('rect');
+	
+	var boxes = svgfile.getElementsByTagName('rect');
 	
 	// store hex colors in array
 	for (var i = 0; i < boxes.length; i++) {
 		hexcolors[i] = boxes[i].style['fill'];
 		rgbcolors.push(hext2rgb(hexcolors[i]));
 	}
-	console.log(writeGPLFormat("test", rgbcolors));
-//	document.getElementById("debug").value = writeGPLFormat("test", rgbcolors);
+	console.log(rgbcolors);
+	
 
 }
 
 
 function hext2rgb(h) {
-	// expecting the hex value to start with '#'. if it be like it don't, shit the bed and go home unseen.
+	// expecting the hex (h) value to start with '#'. if it be like it don't, shit the bed and go home unseen.
 	/*
 	if(h.substring(0,1) !== '#') {
 		console.log(h);
@@ -28,14 +39,17 @@ function hext2rgb(h) {
 		return;
 	}
 	*/
-	console.log(h);
 
 	h = h.substring(1,6);
 
 	var r = parseInt(h.substring(0,2),16);
 	var g = parseInt(h.substring(2,4),16);
 	var b = parseInt(h.substring(4,6),16);
-	return r + ' ' + g + ' ' + b;
+	return [r, g, b];
+}
+
+function drawHTMLPreview() {
+
 }
 
 function writeGPLFormat(name, colors){
@@ -45,15 +59,39 @@ function writeGPLFormat(name, colors){
 
 	fileheader += 'GIMP Palette' + '\n';
 	fileheader += 'Name: ' + name + '\n';
-	fileheader += 'Columns: ' + name + '\n';
+	fileheader += 'Columns: ' + 0 + '\n';
 	fileheader += '#\n';
 
 	for (var i=0; i<colors.length; i++) {
-		console.log(typeof colors[i]);
-		colorvalues += colors[i] + '\n'; //NaN ??
+		colorvalues += "<span style='background: rgba(";
+		for(var r=0; r<colors[i].length; r++) {
+			colorvalues += colors[i][r] + ', ';
+		}
+		colorvalues += "1);'>";
+//		colorvalues += "<span style='background: rgb(" + colors[0] + ");'>";
+//		colorvalues += colors[0] + colors[1] + colors[2] + '<br />'; //NaN ??
+		colorvalues += "</span>";
 	}
-
+	//FIXME REPAIR HELP
 	output = fileheader + colorvalues;
 	return output;
 
-}		
+}
+
+function loadFile(svgfileurl){
+	var url = (svgfileurl);
+	console.log(url);
+	$.get( url, function( data ) {
+	  //process data;
+	  convertColors(data);
+	  setTimeout(function(){
+	  			savePalette();
+	  		}, 500);
+	});
+	
+}
+
+function savePalette() {
+	palettename =  $('#paletteName').val();
+	$("#debug").html(writeGPLFormat(palettename, rgbcolors));
+}
